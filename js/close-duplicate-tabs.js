f400264610231cbd8991f6a36490213877423c35
Sriram Y <sriramy2001@gmail.com>
2023-06-11 23:08:22 -0700

    Close duplicate tabs feature complete (untested)

---
+++ js/close-duplicate-tabs.js
@@ -0,0 +1,48 @@
+import { getAllTabsInCurrentWindow } from "./utils.js";
+
+var closeDuplicatesButton = document.getElementById("closeDuplicates");
+
+closeDuplicatesButton.addEventListener("click", async function() {
+    if (window.confirm("This action will close duplicate tabs in the current window (preserving only ones in tab groups). Are you sure you want to continue?")) {
+        console.log("User wants to proceed with closing duplicate tabs.");
+        await closeDuplicateTabs();
+    } else {
+        console.log("User is aborting closing duplicate tabs.");
+    }
+});
+
+async function closeDuplicateTabs() {
+    try {
+        const allTabsInWindow = await getAllTabsInCurrentWindow();
+        
+        // Resolving the returned promise to a list of tabObjects
+        var tabObjects = allTabsInWindow.map(tab => ({ id: tab.id, title: tab.title, url: tab.url, groupId: tab.groupId }));
+
+        // Remove tabObjects with groupId not equal to -1. We only close duplicate tabs that are outside groups.
+        tabObjects = tabObjects.filter(tab => tab.groupId === -1);
+
+        var urls = [];
+        var duplicates = [];
+
+        // Finding duplicate tabs in tabObjects list
+        tabObjects.forEach(function(tab) {
+            if (!urls.includes(tab.url)) {
+                urls.push(tab.url);
+            } else {
+                duplicates.push(tab);
+                console.log(tab.url);
+            }
+        });
+
+        console.log("\nDUPLICATE TABS");
+        await Promise.all(
+            duplicates.map(async function(tab) {
+                console.log(tab.url); // Debug print
+                await chrome.tabs.remove(tab.id); // Closing the tab
+            })
+        );
+    }
+    catch (error) {
+        console.error("An error occurred while closing duplicate tabs:", error);
+    }
+}
