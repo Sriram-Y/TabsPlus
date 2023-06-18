3c27b1d7aca21a4e669019856f3541f6f59985f8
nimaem77 <nimaem77@gmail.com>
2023-06-18 17:02:14 -0400

    Moved event listener and updated parentId

--- js/create-bookmarks-folder-from-window.js
+++ js/create-bookmarks-folder-from-window.js
@@ -1,19 +1,30 @@
 import { getAllTabsInCurrentWindow } from "./utils.js";
 
+// Add onclick event to trigger the method
+const button = document.getElementById("windowToBookmarksFolder");
+button.addEventListener("click", createBookmarkFromWindow);
+
 async function createBookmarkFromWindow() {
   const allTabsInWindow = await getAllTabsInCurrentWindow();
 
   // Create a bookmarks folder with the tabs
-  chrome.bookmarks.create({ 'title': 'My Tabs Folder', 'index': 0 }, function (newFolder) {
-    allTabsInWindow.forEach(function (tab) {
-      console.log("Hello, World!");
+  chrome.bookmarks.create(
+    { 
+      'title': 'My Tabs Folder', 
+      'parentId': '1' 
+    }, 
+    function (newFolder) {
+      allTabsInWindow.forEach(function (tab) {
+        console.log("Hello, World!");
 
-      chrome.bookmarks.create({ 'parentId': newFolder.id, 'title': tab.title, 'url': tab.url });
-    });
-  });
+        chrome.bookmarks.create(
+          { 
+            'parentId': newFolder.id, 
+            'title': tab.title, 
+            'url': tab.url 
+          }
+        );
+      });
+    }
+  );
 }
-
-// Add onclick event to trigger the method
-const button = document.getElementById("windowToBookmarksFolder");
-button.addEventListener("click", createBookmarkFromWindow);
- 
