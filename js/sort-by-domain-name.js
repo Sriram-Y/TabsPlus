82de6de0f9e8c17f9b62f8d39a580f370ae3609f
mareed2 <apollom8@gmail.com>
2023-06-23 18:44:08 -0400

    commit

--- js/sort-by-domain-name.js
+++ js/sort-by-domain-name.js
@@ -2,19 +2,21 @@ import { getAllTabsInCurrentWindow } from "./utils.js";
 
 var sortByDomainNameButton = document.getElementById("sortByDomainName");
 
-sortByDomainNameButton.addEventListener("click", function() {
+sortByDomainNameButton.addEventListener("click", async function() {
     if (window.confirm("This action will sort all tabs by domain name in the current window. Are you sure you want to continue?")) {
         console.log("User wants to proceed with sorting tabs by domain.");
-        sortTabsByDomain();
+        await sortTabsByDomain();
     } else {
         console.log("User is aborting sorting tabs by domain.");
     }
 });
 
-function getTabsByDomain(callback) {
-    let tabMap = new Map();
-    
-    chrome.tabs.query({currentWindow: true}, allTabsInWindow => {
+async function sortTabsByDomain() {
+    try {
+        const allTabsInWindow = await getAllTabsInCurrentWindow();
+
+        let tabMap = new Map();
+
         allTabsInWindow.forEach(tab => {
             const url = new URL(tab.url);
             const domain = url.hostname;
@@ -25,23 +27,25 @@ function getTabsByDomain(callback) {
             tabMap.get(domain).push(tab);
         });
 
-        callback(tabMap);
-    });
-}
+        // Convert the map entries to an array and sort it
+        const sortedEntries = Array.from(tabMap.entries()).sort((a, b) => a[0].localeCompare(b[0]));
 
-function sortTabsByDomain() {
-    getTabsByDomain(tabsByDomain => {
         let sortedTabIds = [];
-        
-        for (let [domain, tabs] of tabsByDomain) {
+
+        // Iterate over the sorted array to get the sorted tab IDs
+        for (let [domain, tabs] of sortedEntries) {
             const tabIds = tabs.map(tab => tab.id);
             sortedTabIds = sortedTabIds.concat(tabIds);
         }
 
+        // Move the tabs to their sorted positions
         for (let index = 0; index < sortedTabIds.length; index++) {
-            chrome.tabs.move(sortedTabIds[index], {index});
+            await chrome.tabs.move(sortedTabIds[index], {index});
         }
 
         console.log('Tabs have been sorted by domain name');
-    });
+    }
+    catch (error) {
+        console.error("An error occurred while sorting tabs by domain:", error);
+    }
 }
