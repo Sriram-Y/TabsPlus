fae04bf6d43392dcdf021ea0fb55d3a697ed7842
Sriram-Y <sriramy2001@gmail.com>
2023-06-26 23:43:20 -0700

    added functions to get the titles and ids of tabs and tabroups

--- js/utils.js
+++ js/utils.js
@@ -3,10 +3,42 @@ export async function getAllTabsInCurrentWindow() {
         currentWindow: true
     });
 
-    console.log("IN CURRENT WINDOW");
-    allTabsInWindow.forEach(function(tab) {
-        console.log(tab.url);   // debug print
-    });
-
     return allTabsInWindow;
+}
+
+export async function getAllTabGroupsInCurrentWindow() {
+    const allTabGroupsInCurrentWindow = chrome.tabGroups.query({});
+
+    return allTabGroupsInCurrentWindow;
+}
+
+const allTabsInCurrentWindow = await getAllTabsInCurrentWindow();
+const allTabGroupsInCurrentWindow = await getAllTabGroupsInCurrentWindow();
+
+export async function getTabIds() {  // Used for getting a specific tab
+    var tabIdsList = allTabsInCurrentWindow.map(tab => tab.id);
+    
+    console.log("%c" + tabIdsList, "color: red;");
+    return tabIdsList;
+}
+
+export async function getTabTitles() {
+    var tabTitlesList = allTabsInCurrentWindow.map(tab => tab.title);
+
+    console.log("%c" + tabTitlesList, "color: red;");
+    return tabTitlesList;
+}
+
+export async function getTabGroupIds() {
+    var tabGroupIdsList = allTabGroupsInCurrentWindow.map(tabGroup => tabGroup.id);
+
+    console.log("%c" + tabGroupIdsList, "color: red;");
+    return tabGroupIdsList;
+}
+
+export async function getTabGroupTitles() {
+    var tabGroupTitlesList = allTabGroupsInCurrentWindow.map(tabGroup => tabGroup.title);
+
+    console.log("%c" + tabGroupTitlesList, "color: red;");
+    return tabGroupTitlesList;
 }
