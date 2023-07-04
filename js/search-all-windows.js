a763a8adc9f540c07f91befc211094054209c0b0
Sriram-Y <sriramy2001@gmail.com>
2023-07-04 15:29:49 -0700

    added warning for search all

--- js/search-all-windows.js
+++ js/search-all-windows.js
@@ -10,81 +10,87 @@ import { getTabIds, getTabTitleFromTabId } from "./utils.js"
 var searchAllWindowsButton = document.getElementById("searchAllWindows");
 
 searchAllWindowsButton.addEventListener("click", async function () {
-    try {
-        var stringFoundTabIds = [];
-        var searchString = prompt("What would you like to search?");
-
-        if (searchString != null && searchString.length != 0) {
-            try {
-                loadAllTabsInWindow();
-            }
-            catch (loadingError) {
-                console.error("An error occurred while loading all tabs (preparing for search):", error);
-                alert("There was an error searching through all the tabs.")
-            }
-
-            const allTabsIdsInWindow = await getTabIds();
-            await allTabsIdsInWindow;
-
-            for (let i = 0; i < allTabsIdsInWindow.length; i++) {
-                var tabObject = await chrome.tabs.get(allTabsIdsInWindow[i]);
-                if (tabObject.url.includes('chrome://')) {
-                    allTabsIdsInWindow.splice(i, 1);
+    if (window.confirm("This action will reload all tabs in the window. Are you sure you want to continue?")) {
+        console.log("User wants to proceed with searching all tabs.");
+        try {
+            var stringFoundTabIds = [];
+            var searchString = prompt("What would you like to search?");
+    
+            if (searchString != null && searchString.length != 0) {
+                try {
+                    loadAllTabsInWindow();
                 }
-            }
-
-            for (let i = 0; i < allTabsIdsInWindow.length; i++) {
-                stringFoundTabIds.push(await searchInTab(allTabsIdsInWindow[i], searchString));
-            }
-
-            // Removing items equal to 0
-            stringFoundTabIds = stringFoundTabIds.filter(item => item !== 0);
-            // Removing duplicate items in list
-            stringFoundTabIds = stringFoundTabIds.filter(item => stringFoundTabIds.indexOf(item) === stringFoundTabIds.lastIndexOf(item));
-
-            // Now, bring into view the sub menu with titles appearing in a list
-            const menuItemsElement = document.getElementById("search-all-windows-menu");
-            menuItemsElement.innerHTML = "";    // clear existing menu items
-
-            if (stringFoundTabIds.length > 0) {
-                stringFoundTabIds.forEach(async function (id) {
-                    await getTabTitleFromTabId(id).then(async function (result) {
-                        const listItem = document.createElement("li");
-                        listItem.id = "tab-item";
-                        listItem.onmouseover = function () {
-                            listItem.style.color = "blue";
-                        };
-                        listItem.onmouseout = function () {
-                            listItem.style.color = "";
-                        };
-                        listItem.textContent = result;
-                        listItem.addEventListener("click", function () {
-                            // await moveAndFind(id, searchString);
-                            chrome.runtime.sendMessage({
-                                message: [id, searchString]
+                catch (loadingError) {
+                    console.error("An error occurred while loading all tabs (preparing for search):", error);
+                    alert("There was an error searching through all the tabs.")
+                }
+    
+                const allTabsIdsInWindow = await getTabIds();
+                await allTabsIdsInWindow;
+    
+                for (let i = 0; i < allTabsIdsInWindow.length; i++) {
+                    var tabObject = await chrome.tabs.get(allTabsIdsInWindow[i]);
+                    if (tabObject.url.includes('chrome://')) {
+                        allTabsIdsInWindow.splice(i, 1);
+                    }
+                }
+    
+                for (let i = 0; i < allTabsIdsInWindow.length; i++) {
+                    stringFoundTabIds.push(await searchInTab(allTabsIdsInWindow[i], searchString));
+                }
+    
+                // Removing items equal to 0
+                stringFoundTabIds = stringFoundTabIds.filter(item => item !== 0);
+                // Removing duplicate items in list
+                stringFoundTabIds = stringFoundTabIds.filter(item => stringFoundTabIds.indexOf(item) === stringFoundTabIds.lastIndexOf(item));
+    
+                // Now, bring into view the sub menu with titles appearing in a list
+                const menuItemsElement = document.getElementById("search-all-windows-menu");
+                menuItemsElement.innerHTML = "";    // clear existing menu items
+    
+                if (stringFoundTabIds.length > 0) {
+                    stringFoundTabIds.forEach(async function (id) {
+                        await getTabTitleFromTabId(id).then(async function (result) {
+                            const listItem = document.createElement("li");
+                            listItem.id = "tab-item";
+                            listItem.onmouseover = function () {
+                                listItem.style.color = "blue";
+                            };
+                            listItem.onmouseout = function () {
+                                listItem.style.color = "";
+                            };
+                            listItem.textContent = result;
+                            listItem.addEventListener("click", function () {
+                                // await moveAndFind(id, searchString);
+                                chrome.runtime.sendMessage({
+                                    message: [id, searchString]
+                                });
                             });
+                            menuItemsElement.appendChild(listItem);
                         });
-                        menuItemsElement.appendChild(listItem);
                     });
-                });
+                }
+                else {
+                    const listItem = document.createElement("li");
+                    listItem.id = "tab-item";
+                    listItem.style.color = "red";
+                    listItem.textContent = "No results for the search term were found.";
+    
+                    menuItemsElement.appendChild(listItem);
+                }
+    
+                menuItemsElement.style.display = "block";
             }
             else {
-                const listItem = document.createElement("li");
-                listItem.id = "tab-item";
-                listItem.style.color = "red";
-                listItem.textContent = "No results for the search term were found.";
-
-                menuItemsElement.appendChild(listItem);
+                alert("Please enter a search term.");
             }
-
-            menuItemsElement.style.display = "block";
         }
-        else {
-            alert("Please enter a search term.");
+        catch (error) {
+            console.error("An error occurred while searching all windows:", error);
         }
-    }
-    catch (error) {
-        console.error("An error occurred while searching all windows:", error);
+    } 
+    else {
+        console.log("User is aborting searching all tabs.");
     }
 });
 
